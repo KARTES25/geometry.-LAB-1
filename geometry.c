d3b739d73a507cd6dec4fa722b8feb0db6dd8103
Андреев Алексей Александрович <ip812s01@mwp03.csc.local>
2019-03-25 12:44:40 +0700

    chenge code

--- geometry.c
+++ geometry.c
@@ -6,12 +6,12 @@ const float p = 3.1415912;
 
 int circle(int x1, int y1, float r1)
 {
-    printf("circle1(%d %d, %.3f)\n", x1, y1, r1);
+    cout<<"circle1"<< x1<< y1<< r1;
     float s1, p1;
     p1 = 2 * p * r1;
     s1 = p * r1 * r1;
-    printf("perimetr = %.3f\n", p1);
-    printf("area = %.3f\n", s1);
+    cout<<"perimetr = "<< p1<< endl;
+    cout<<"area = "<< s1<<endl;
     return 0;
 }
 
@@ -20,14 +20,14 @@ int main()
     int x1, y1;
     float r1;
     cout << "Enter parametr of 1st circle(WKT)" << endl;
-    scanf("%d%d%f", &x1, &y1, &r1);
+    cin>>x1>>y1>>r1;
     if (r1 <= 0) {
-        cout << "Error:Circle can't have negative radius\n" << endl;
+        cout << "Error:Circle can't have negative radius" << endl;
     }
     int x2, y2;
     float r2;
     cout << "Enter parametr of 2nd circle(WKT)" << endl;
-    scanf("%d%d%f", &x2, &y2, &r2);
+    cin>>x2>>y2>>r2;
     if (r2 <= 0) {
         cout << "Error:Circle can't have negative radius\n" << endl;
     }
